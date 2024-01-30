62cfdcc6fbc9f7f68e855d16da1356c082f97ad9
magedsaif <magedsaif21@gmail.com>
2024-01-30 12:48:33 +0200

    task 0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt="logo" className="logo"></img>
+        <h1>School dashboard</h1>
+      </div>
+      <hr className="myHr" />
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <hr className="myHr" />
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
