c66b8167e3bc2276affa4527be514c7efe9d54a7
Maged Saif <magedsaif21@gmail.com>
2024-02-04 19:41:36 +0200

    first commit

--- 0x02-react_props/task_0/dashboard/src/APP/App.js
+++ 0x02-react_props/task_0/dashboard/src/APP/App.js
@@ -1,38 +1,20 @@
-import logo from "./holberton-logo.jpg";
-import "./App.css";
-import { getFooterCopy, getFullYear } from "./utilis";
-import Notifications from "./Notifications";
-import Header from "../Header/Header";
+import React from 'react';
+import './App.css';
+import Notifications from '..Notifications/Notifications';
+import Login from '../Login/Login'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <Notifications />
-      <Header></Header>
-      <body className="App-body">
-        <p>Login to access the full dashboard</p>
-
-        <form>
-          {/* Email */}
-          <div>
-            <label htmlFor="email">Email: </label>
-            <input type="email" id="email" />
-          </div>
-
-          {/* Password Input */}
-          <div>
-            <label htmlFor="password">Password: </label>
-            <input type="password" id="password" />
-          </div>
-
-          {/* OK Button */}
-          <button>OK</button>
-        </form>
-      </body>
-      <footer className="App-footer">
-        <p>{`${getFooterCopy(true)} - ${getFullYear()}`}</p>
-      </footer>
-    </div>
+      <div className="App">        
+        <Header />        
+          <Login />       
+          <Footer />             
+      </div>
+    </React.Fragment>
   );
 }
 
--- 0x02-react_props/task_0/dashboard/src/Header/Header.js
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -2,13 +2,15 @@ import React from "react";
 import logo from "../assets/holberton-logo.jpg";
 import "./Header.css";
 
-export default function Header() {
+function Header() {
   return (
     <>
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
-      </header>
+      </div>
     </>
   );
 }
+
+export default Header;
